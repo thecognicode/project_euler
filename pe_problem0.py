44ef91e2498a769525652058787c13dfd2ee5242
TheCogniCode <theabisheksam@gmail.com>
2026-03-04 23:58:50 +0530

    Problem 0 - Brute force

--- pe_problem0.py
+++ pe_problem0.py
@@ -1,29 +1,7 @@
-# n = 100
-# add = 0
-# for i in range(1,n+1):
-#     square = i ** 2
-#     # print(square, end=' ')
-#     if square % 2 != 0:
-#         # print(square, end=' ')
+n = 5
+sum = 0
 
-#         add += square
-# print(add)
-
-# sum = 0
-# for i in range(0, n+1):
-#     if i % 2 != 0:
-#         sum += i**2
-# print(sum)
-
-n = 100
-square_sum = 0
-sum_square = 0 
-all_sum = 0
-
-for i in range(1,n+1):
-    square_sum += i ** 2
-    all_sum += i
-    sum_square = all_sum ** 2
-
-# print(all_sum)
-print(sum_square-square_sum)
+for i in range(0, n+1):
+    if i % 2 != 0:
+        sum += i**2
+print(sum)
